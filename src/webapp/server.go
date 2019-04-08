6b8811696bcb8e4e27c44e7f50310af9c7d0d558
wdkkou <koukouki7547@gmail.com>
2019-04-08 21:42:43 +0900

    server.go handle

--- src/webapp/server.go
+++ src/webapp/server.go
@@ -5,18 +5,38 @@ import (
 	"net/http"
 )
 
+// MyHandler is struct
 type MyHandler struct{}
 
 func (h *MyHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello World!")
+	fmt.Fprintf(w, "handle!")
+}
+
+// HelloHandler is struct
+type HelloHandler struct{}
+
+func (h *HelloHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello!")
+}
+
+// Wdk is struct
+type Wdk struct{}
+
+func (h *Wdk) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "wdkkou!")
 }
 
 func main() {
 	handler := MyHandler{} // handlerはハンドラ。
+	hello := HelloHandler{}
+	wdkkou := Wdk{}
 	server := http.Server{
 		Addr: "127.0.0.1:8080",
 		//		Addr:    ":8080", // これでもOK
-		Handler: &handler,
 	}
+
+	http.Handle("/", &handler)
+	http.Handle("/hello", &hello)
+	http.Handle("/wdkkou", &wdkkou)
 	server.ListenAndServe()
 }
